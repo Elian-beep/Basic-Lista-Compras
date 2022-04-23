81ccd11dbead0d002ad3b27fefd814caee43412b
Elian-beep <elianoliver101@gmail.com>
2022-04-22 23:15:06 -0300

    js: inclusão e exclusão dos itens no local storage

--- list.js
+++ list.js
@@ -4,10 +4,26 @@ let novoItem = document.querySelector('#novoItem');
 let btnNovoItem = document.querySelector('#btnNovoItem');
 let items = document.querySelector('#items');
 
+recuperarLista(); //Verificando se há dados salvos no storage
+
 btnNovoItem.addEventListener('click', (e) => {
-    inserirItem({ nome: novoItem.value, id: null }); //Insere um objeto na função
+    inserirItem({ nome: novoItem.value, id: gerarId() }); //Insere um objeto na função
     novoItem.value = '';
 })
+
+
+
+//------------------------------FUNÇÃO PARA RECUPERAR OS DADOS DA LISTA
+function recuperarLista(){
+    let listaItems = localStorage.getItem('listaDeCompras'); //Recupera as informações salvas no local storage do navgeador
+    if(listaItems){ //Se houer items para serem recuperados
+        let items = JSON.parse(listaItems); //Recupera as strings salvas convertendo para um JSON
+
+        for(const item of items){ //Para cada item do JSON
+            inserirItem(item, false); //Inserir um novo html como item ja existente
+        }
+    }
+}
 
 
 
@@ -24,7 +40,7 @@ function inserirItem(item, novoItem = true){//Se não houver um novo item, a var
     items.appendChild(criarItemLista(item)); //Adiciona uma nova tag filha na lista
 
     if(novoItem){ //Se for um novo item
-        sessionStorage.setItem('listaDeCompras', JSON.stringify(lista)); //Armazenando no Storage o novo item da lista
+        localStorage.setItem('listaDeCompras', JSON.stringify(lista)); //Armazenando no Storage o novo item da lista
     }
 }
 
@@ -40,4 +56,20 @@ function criarItemLista(item){
     li.id = item.id;
 
     return li;
+}
+
+
+
+//------------------------------FUNÇÃO PARA REMOVER O ITEM DA LISTAE DO STORAGE
+function deletarItem(id){
+    let indice = lista.findIndex(i => i.id == id); //Vai procurar o indice do elemento a ser apagado na lista de items (lista criada no incio do cod)
+
+    if(indice < 0){
+        alert('O indice não foi encontrado');
+        return;
+    }
+
+    lista.splice(indice, 1); //Removendo APENAS o elemento com o indice encontrado
+    localStorage.setItem('listaDeCompras', JSON.stringify(lista)); // Atualiza a lista no storage
+    document.getElementById(''+id+'').remove(); //Remove o elemento html do item
 }
